fdca431d754ed662be9d49f7cb7f2727fa7f197f
Komla Sanussi <your.kamilousanussi@gmail.com>
2025-12-13 23:24:35 +0000

    Connect keypad input to amount state

---
+++ src/components/AmountDisplay.jsx
@@ -0,0 +1,9 @@
+function AmountDisplay({ value }) {
+  return (
+    <div className="text-right text-4xl font-bold border-b-2 border-pink-400 py-2">
+      {value}
+    </div>
+  );
+}
+
+export default AmountDisplay;
