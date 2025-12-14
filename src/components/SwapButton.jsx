be3fe56cb9719eae0d4b16597a023ace65dd794e
Komla Sanussi <your.kamilousanussi@gmail.com>
2025-12-14 16:27:29 +0000

    Finalize currency converter UI and functionality

--- src/components/SwapButton.jsx
+++ src/components/SwapButton.jsx
@@ -1,6 +1,23 @@
-function SwapButton() {
+function SwapButton({ onClick }) {
   return (
-    <button className="bg-purple-950 px-4 py-2 rounded-full text-white font-semibold">
+    <button
+      onClick={onClick}
+      className="
+        mx-auto
+        w-10
+        h-10
+        flex
+        items-center
+        justify-center
+        rounded-full
+        bg-purple-800
+        text-white
+        text-xl
+        hover:bg-purple-600
+        active:scale-90
+        transition
+      "
+    >
       ⇅
     </button>
   );
